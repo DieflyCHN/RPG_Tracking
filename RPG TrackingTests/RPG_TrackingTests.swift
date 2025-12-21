d11f18038cf656cd6e6258e7179c2578f10d6ec8
DieflyCHN <taoyi.sh.cn@gmail.com>
2025-12-21 08:17:14 +0100

    Initial Commit

---
+++ RPG TrackingTests/RPG_TrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPG_TrackingTests.swift
+//  RPG TrackingTests
+//
+//  Created by Kornelius Schneider on 21.12.25.
+//
+
+import Testing
+@testable import RPG_Tracking
+
+struct RPG_TrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ RPG TrackingUITests/RPG_TrackingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RPG_TrackingUITestsLaunchTests.swift
+//  RPG TrackingUITests
+//
+//  Created by Kornelius Schneider on 21.12.25.
+//
+
+import XCTest
+
+final class RPG_TrackingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
